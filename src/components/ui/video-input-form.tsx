d16d5c58d05ecca7cbce891d0545595e2182e302
alissonjosef <alissonjosef@gmail.com>
2023-09-14 21:45:39 -0300

    ffmpeg coverte audio

--- src/components/ui/video-input-form.tsx
+++ src/components/ui/video-input-form.tsx
@@ -4,10 +4,12 @@ import { Button } from "./button";
 import { Separator } from "@radix-ui/react-separator";
 import { Textarea } from "./textarea";
 import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react";
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 export function VideoInputForm() {
   const [videoFile, setVideoFile] = useState<File | null>(null);
-  const promptInputRef = useRef<HTMLTextAreaElement>(null)
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
 
   function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
     const { files } = event.currentTarget;
@@ -20,16 +22,62 @@ export function VideoInputForm() {
     setVideoFile(selectedFile);
   }
 
-  function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+  async function convertVideoToAudio(video: File) {
+    console.log("Coverte start");
 
-    const prompt = promptInputRef.current?.value
+    const ffmpeg = await getFFmpeg();
 
-    if(!videoFile){
-      return
+    await ffmpeg?.writeFile("input.mp4", await fetchFile(video));
+
+    /*   ffmpeg?.on('log', log => {
+      console.log("🚀 ~ file: video-input-form.tsx:34 ~ convertVideoToAudio ~ log:", log)
+      
+    }) */
+
+    ffmpeg?.on("progress", (progress) => {
+      console.log("Convert progress: " + Math.round(progress.progress * 100));
+    });
+
+    await ffmpeg?.exec([
+      "-i",
+      "input.mp4",
+      "-map",
+      "0:a",
+      "-b:a",
+      "20k",
+      "-acodec",
+      "libmp3lame",
+      "output.mp3",
+    ]);
+
+    const data = await ffmpeg?.readFile("output.mp3");
+
+    const audioFileBlob = new Blob([data], { type: "audio/mpeg" });
+    const audioFile = new File([audioFileBlob], "audio.mpp3", {
+      type: "audio/mpeg",
+    });
+
+    console.log("Convert Finished");
+
+    return audioFile;
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) {
+      return;
     }
 
-    
+    const audioFile = await convertVideoToAudio(videoFile);
+
+    console.log(
+      "🚀 ~ file: video-input-form.tsx:78 ~ handleUploadVideo ~ audioFile:",
+      audioFile,
+      prompt
+    );
   }
 
   const previewURL = useMemo(() => {
